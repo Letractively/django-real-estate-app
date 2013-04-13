aa48dda85380198d386fe717a9a47807ffe9d76d
mbsalgueiro <mbsalgueiro@linuxmail.org>
2013-04-13 03:39:58 +0000

    Removed a wrong trunk created on r31

--- trunk/urls/contact.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import url
-
-contact = url(
-	regex  = '^contact/$',
-	view   = 'real_estate_app.views.contact',
-	name   = 'contact'
-)
--- trunk/urls/share.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import url
-
-send_content = url(
-	regex  = '^send-content/$',
-	view   = 'real_estate_app.views.send_content',
-	name   = 'share-send-content'
-)
